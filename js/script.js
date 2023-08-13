3bf9438b47c47e11bf434dd83eec167a64de8c6f
sollendy <edlannino@tutanota.com>
2023-08-13 18:13:42 +0200

    preparato foglio js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const API_BASE_URL = 'https://api.openai.com/v1';
 const API_KEY = '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'; // Inserisci qui il valore della API_KEY
 const GPT_MODEL = 'gpt-3.5-turbo';
+
+// ricaviamo gli elementi dalla pagina
+
+const loader = document.querySelector('.carico');
+const genreButtons = document.querySelectorAll('.genere');
+const placeholder = document.querySelector('#segnaparte');
+const stageTemplate = document.querySelector('#segna-quadro');
+const gameoverTemplate = document.querySelector('#segna-fine');
