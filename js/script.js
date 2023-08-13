bc9ca433e91a5927c22f4d0c5b0e1a3029901c0a
sollendy <edlannino@tutanota.com>
2023-08-13 18:22:31 +0200

    rimossa per la miliardesima volta la chiave api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = 'https://api.openai.com/v1';
-const API_KEY = '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'; // Inserisci qui il valore della API_KEY
+const API_KEY = ''; // Inserisci qui il valore della API_KEY
 const GPT_MODEL = 'gpt-3.5-turbo';
 
 // ricaviamo gli elementi dalla pagina
