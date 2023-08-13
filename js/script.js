4e0e4b546ec0422ff11f8cd3fcbb3b9e6d9dfa79
sollendy <edlannino@tutanota.com>
2023-08-13 17:26:57 +0200

    terminata parte statica

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = 'https://api.openai.com/v1';
+const API_KEY = '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'; // Inserisci qui il valore della API_KEY
+const GPT_MODEL = 'gpt-3.5-turbo';
